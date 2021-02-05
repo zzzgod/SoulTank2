c4c02babc05f18337d59d9ec9208d2432fae6d4a
zzz <845538195@qq.com>
2021-02-05 13:45:07 +0800

    123

---
+++ assets/Script/Tank.ts
@@ -0,0 +1,107 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    currentSpeed: number = 0;
+
+    currentAngularSpeed: number = 0;
+
+    @property({displayName:'前向加速度'})
+    forwardAcceleration: number = 0.4;
+
+    @property({displayName:'后向加速度'})
+    backwardAcceleration: number = 0.1;
+
+    @property({displayName:'前向最大速度'})
+    forwardSpeed: number = 4;
+
+    @property({displayName:'后向最大速度'})
+    backwardSpeed: number = 1;
+    
+    @property({displayName:'旋转角速度'})
+    angularSpeed: number = 1;
+    
+    key_w_pressed: boolean = false;
+    key_a_pressed: boolean = false;
+    key_s_pressed: boolean = false;
+    key_d_pressed: boolean = false;
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    }
+
+    start () {
+        cc.log('jdadjajdjoaopdopadpoaopdkopadkopkaopdkopadopakdodkpoakdposopkdpak');
+    }
+
+    update (dt) {
+        this.move();
+    }
+
+    onKeyDown(e: cc.Event.EventKeyboard){
+        switch(e.keyCode){
+            case cc.macro.KEY.w:
+                this.key_w_pressed = true;
+                break;
+            case cc.macro.KEY.a:
+                this.key_a_pressed = true;
+                break;
+            case cc.macro.KEY.s:
+                this.key_s_pressed = true;
+                break;
+            case cc.macro.KEY.d:
+                this.key_d_pressed = true;
+                break;
+        }
+    }
+
+    onKeyUp(e: cc.Event.EventKeyboard){
+        switch(e.keyCode){
+            case cc.macro.KEY.w:
+                this.key_w_pressed = false;
+                break;
+            case cc.macro.KEY.a:
+                this.key_a_pressed = false;
+                break;
+            case cc.macro.KEY.s:
+                this.key_s_pressed = false;
+                break;
+            case cc.macro.KEY.d:
+                this.key_d_pressed = false;
+                break;
+        }
+    }
+
+    move(){
+        this.currentAngularSpeed = 0;
+        if(this.key_w_pressed && !this.key_s_pressed){
+            // 前向加速或保持匀速
+            if(this.currentSpeed < this.forwardSpeed)
+                this.currentSpeed = Math.min(this.currentSpeed + this.forwardAcceleration, this.forwardSpeed)
+        }
+        else if(!this.key_w_pressed && this.key_s_pressed){
+            // 后向加速或保持匀速
+            if(this.currentSpeed > -this.backwardSpeed)
+                this.currentSpeed = Math.max(this.currentSpeed - this.backwardAcceleration, -this.backwardSpeed)
+        }
+        if(this.key_a_pressed && !this.key_d_pressed){
+            this.currentAngularSpeed = this.angularSpeed;
+        }
+        else if(!this.key_a_pressed && this.key_d_pressed){
+            this.currentAngularSpeed = -this.angularSpeed;
+        }
+        this.node.angle += this.currentAngularSpeed;
+        this.node.x += this.currentSpeed * Math.cos(this.node.angle * Math.PI / 180);
+        this.node.y += this.currentSpeed * Math.sin(this.node.angle * Math.PI / 180);
+    }
+}
---
+++ assets/Script/Turret.ts
@@ -0,0 +1,53 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    // LIFE-CYCLE CALLBACKS:
+
+    @property({displayName:'旋转角速度'})
+    angularSpeed: number = 1;
+
+    mouseLocation: cc.Vec2 = cc.v2();
+
+    onLoad () {
+        this.node.parent.parent.on(cc.Node.EventType.MOUSE_MOVE, this.onMouseMove, this);
+    }
+
+    start () {
+
+    }
+
+    update (dt) {
+        let direction: cc.Vec2 = this.mouseLocation.sub(this.node.parent.getPosition());
+        // 鼠标相对父节点的角度
+        let mouseAngle: number = Math.atan2(direction.y, direction.x) * 180 / Math.PI - this.node.parent.angle;
+        // 计算需要转动的角度[0~360]，0~180正转，180~360反转
+        let delta: number = (mouseAngle - this.node.angle + 360) % 360;
+        if(delta <= this.angularSpeed){
+            this.node.angle = mouseAngle;
+        }
+        else if(delta >= 360 - this.angularSpeed){
+            this.node.angle = mouseAngle;
+        }
+        else if(delta < 180){
+            // 抵消坦克自转的角速度
+            this.node.angle += this.angularSpeed - this.node.parent.getComponent('Tank').currentAngularSpeed;
+        }
+        else{
+            this.node.angle -= this.angularSpeed + this.node.parent.getComponent('Tank').currentAngularSpeed;
+        }
+    }
+
+    onMouseMove(e: cc.Event.EventMouse) {
+        // 将鼠标的绝对坐标转换为相对于画布的坐标系
+        this.mouseLocation = this.node.parent.parent.convertToNodeSpaceAR(e.getLocation())
+    }
+}
